50fc4bdfeff195ab98e4a683ee319f5303628966
Sally Xie <sally_xie@cmoney.com.tw>
2022-04-01 14:01:09 +0800

    Initial Commit

---
+++ Shared/MyResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyResumeApp.swift
+//  Shared
+//
+//  Created by cm0676 on 2022/4/1.
+//
+
+import SwiftUI
+
+@main
+struct MyResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
